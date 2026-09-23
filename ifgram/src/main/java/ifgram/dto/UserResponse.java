f2f9f58de10dc2fc979c83c28c5bfcdca4ca32ce
Alan da Silva Lima <202412060067@ad.ifpb.edu.br>
2026-09-23 09:51:08 -0300

    Record

--- ifgram/src/main/java/ifgram/dto/UserResponse.java
+++ ifgram/src/main/java/ifgram/dto/UserResponse.java
@@ -1,6 +1,11 @@
 package ifgram.dto;
 
 
+import org.apache.catalina.User;
 
-public record UserResponse() {
+public record UserResponse(Long id, String nome, String email) {
+    public static UserResponse from(User user){
+        return new UserResponse(user.getId(), user.getNome(), user.getEmail());
+    }
+
 }
